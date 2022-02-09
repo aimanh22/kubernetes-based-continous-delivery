b510cdf289fab36d33f4a17eaed3eae8856fb54d
Aiman <69780552+aimanh22@users.noreply.github.com>
2022-02-09 17:50:56 -0500

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -3,6 +3,7 @@ import requests
 import regex as re
 from bs4 import BeautifulSoup
 from sumy.summarizers.text_rank import TextRankSummarizer
+from sumy.summarizers.lex_rank import LexRankSummarizer
 from sumy.parsers.plaintext import PlaintextParser
 from sumy.nlp.tokenizers import Tokenizer
 from sumy.nlp.stemmers import Stemmer
